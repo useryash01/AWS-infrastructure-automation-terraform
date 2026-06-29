799fd9f5bc0d7b5733d12f0a169243749c17d7b0
Yash <yashmayekar650@gmail.com>
2026-06-29 07:40:21 +0000

    add remote backend configuration and github actions pipeline

--- versions.tf
+++ versions.tf
@@ -7,4 +7,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  # Add this backend block right here
+  backend "s3" {
+    bucket         = "yash-terraform-state-bucket-14279"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
 }
